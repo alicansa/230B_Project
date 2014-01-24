e38d44852baa216385167f892d734e45a25f2861
alicansa <alicansa@gmail.com>
2014-01-23 23:46:29 -0800

    -

---
+++ step1/awgn_channel.m
@@ -0,0 +1,7 @@
+function [channel_output] = awgn_channel(transmitted_sig,variance,SNR)
+%AWGN_CHANNEL Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- step1/step1_sim_QAM16.m
+++ step1/step1_sim_QAM16.m
@@ -1,12 +1,15 @@
 close all;
 clear all;
-overSampleSize = 4;
-
-
+overSampleSize = 8;
+rollOffFactor = 0;
+Ts = 1;
 
 %%squareroot raised cosine test
-srrc = sqrt_raised_cosine(8,0.1,4,1);
+srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,400,Ts);
 stem(srrc);
+figure
+stem(conv(srrc,srrc,'same'));
+
 
 %%16-QAM
 N=16;
@@ -19,11 +22,59 @@ test_decode_16 = QAM_16_demod(inphase_16,quadrature_16);
 test_ber_16 = BER(bits_16,test_decode_16);
 
 %%16-QAM simulation
-% bits = random_bit_generator(N);
-% [quadrature inphase] = QAM_16_mod(bits,N);
-% impulse_train_quad__16 = impulse_train(overSampleSize,N,quadrature);
-% impulse_train_inphase_16 = impulse_train(overSampleSize,N,inphase);
 
+%
+%bits_16 = random_bit_generator(N);
+%[quadrature_16 inphase_16] = QAM_16_mod(bits_16,N/4);
+impulse_train_quad_16 = impulse_train(overSampleSize,N,quadrature_16);
+impulse_train_inphase_16 = impulse_train(overSampleSize,N,inphase_16);
+figure;
+stem(impulse_train_quad_16);
+figure;
+stem(impulse_train_inphase_16);
+
+
+
+%
+transmit_quad_16 = conv(impulse_train_quad_16,srrc,'same');
+transmit_inphase_16 = conv(impulse_train_inphase_16,srrc,'same');
+figure;
+stem(transmit_quad_16);
+figure;
+stem(transmit_inphase_16);
+
+%pass through channel
+
+
+
+%matched filter
+matched_output_quad = conv(transmit_quad_16,srrc,'same');
+matched_output_inphase = conv(transmit_inphase_16,srrc,'same');
+
+figure;
+plot(matched_output_quad);
+hold on
+stem(impulse_train_quad_16,'r');
+
+
+figure;
+plot(matched_output_inphase);
+hold on
+stem(impulse_train_inphase_16,'r');
+
+
+%sampler
+sampled_quad = sampler(matched_output_quad,overSampleSize,Ts);
+sampled_inphase = sampler(matched_output_inphase,overSampleSize,Ts);
+
+
+
+
+%decision
+output_bits = QAM_16_demod(sampled_inphase,sampled_quad);
+
+%BER calculation
+ber_16 = BER(bits_16(5:16),output_bits(5:16));
 
 %%64-QAM
 N=64;
@@ -35,7 +86,7 @@ test_decode_64 = QAM_64_demod(inphase_64,quadrature_64);
 test_ber_64 = BER(bits_64,test_decode_64);
 
 %%64-QAM simulation
-% bits = random_bit_generator(N);
-% [quadrature inphase] = QAM_64_mod(bits,N);
-% impulse_train_quad_64 = impulse_train(overSampleSize,N,quadrature);
-% impulse_train_inphase_64 = impulse_train(overSampleSize,N,inphase);
+bits = random_bit_generator(N);
+[quadrature inphase] = QAM_64_mod(bits,N);
+impulse_train_quad_64 = impulse_train(overSampleSize,N,quadrature);
+impulse_train_inphase_64 = impulse_train(overSampleSize,N,inphase);
