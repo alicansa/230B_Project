27d5155fe9c81575939fac60c78008013deaf536
Darren Reis <darren.r.reis@gmail.com>
2014-01-26 23:08:43 -0800

    new files
    new files

---
+++ step1/bpsk_mod.m
@@ -0,0 +1,29 @@
+function [ sym ] = bpsk_mod( bits, N )
+% FUNCTION
+%   convert the bits into symbols on bpsk scheme
+
+% INPUTS
+%   bits - the data input bitstream
+%   N -
+
+% OUTPUTS
+%   sym - the symbol stream after modulation
+
+sym = zeros(1,N);
+
+for i=1:N
+    
+    currentWord = bits(i);
+    
+    switch currentWord
+        
+        case '0'
+            sym(i) = -1;
+        case '1'
+            sym(i) = 1;
+
+    end
+end
+
+end
+
