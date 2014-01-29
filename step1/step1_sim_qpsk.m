ced5907d9eaa7407cb1f501b7bc96fb0d4c31acf
Darren Reis <darren.r.reis@gmail.com>
2014-01-28 19:27:14 -0800

    new graphs

--- step1/step1_sim_qpsk.m
+++ step1/step1_sim_qpsk.m
@@ -4,7 +4,7 @@ overSampleSize = 4;
 rollOffFactor = 0.1;
 Ts = 1;
 srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,400,Ts);
-SNR = [3 4 5 6 7 8 9 10 11 12 13 14 15 16];
+SNR = [3  6  10 15 20];
 
 
 
@@ -46,8 +46,14 @@ transmit_inphase = conv(impulse_train_inphase,srrc,'same');
 
 %loop this section for BER vs SNR graphs
 
+% declare variables
+h = zeros(1,5);
 ber = zeros(1,length(SNR));
-ber_theo = zeros(1,length(SNR));
+ser = zeros(1,length(SNR));
+ber_SNR = zeros(1,length(SNR));
+ber_EbN0 = zeros(1,length(SNR));
+
+f = figure;
 for i=1:length(SNR)
    %pass through awgn channel
     received_quad = awgn_channel(transmit_quad,SNR(i));
@@ -61,27 +67,42 @@ for i=1:length(SNR)
     sampled_quad = sampler(matched_output_quad,overSampleSize,Ts);
     sampled_inphase = sampler(matched_output_inphase,overSampleSize,Ts);
 
+    
+    
+    subplot(2,3,i);
+    scatter(sampled_inphase,sampled_quad);
+    xlim = [1.5*min(sampled_inphase) 1.5*max(sampled_inphase)];
+    ylim = [1.5*min(sampled_quad) 1.5*max(sampled_quad)];
+    line(xlim,[0 0], 'Color', 'k');
+        line([0 0],ylim,'Color', 'k');
+    title(['QPSK Constellation with'...
+        sprintf('\nSNR = %d dB',SNR(i))]);
+    axis([xlim, ylim]);
     %decision
     output_bits = qpsk_demod(sampled_inphase,sampled_quad);
-
+  
     %BER/SER calculation
     ber(i) = BER(bits(3:N),output_bits(3:N));    
     ser(i) = SER(bits,output_bits,k);
     %SER/BER theoretical calculation (BER=SER due to grey coding)
     a = 10^(EbN0(i)/10);
-    ber_theo(i) = 2*qfunc(sqrt(2*a))-qfunc(sqrt(2*a))^2;
+    ber_EbN0(i) = 1/2*erfc(sqrt(a));
+    ber_SNR(i) = 2*qfunc(sqrt(2*a))-qfunc(sqrt(2*a))^2;
 end
+
+
 
 
 %plot theoretical/simulation BER vs SNR graph
 
-h=figure;
-semilogy(SNR,ser,'r');
+g=figure;
+semilogy(SNR,ber,'r');
 hold on;
-%semilogy(SNR,ser, 'b');
-semilogy(SNR,ber_theo, 'g');
+semilogy(SNR,ber_EbN0, 'b');
+semilogy(SNR,ber_SNR, 'g');
 title(['Comparison of Theoretical and Experimental',...
     sprintf('\nQPSK Bit Error Rates')]);
 ylabel('Bit Error Rate (%)');
 xlabel('Signal To Noise (dB)');
+legend('Experimental','Eb/N0','Symbol SNR');
 
