a7e3ca88236d9dd1cdcda1b9f42b72f4a53c2924
Darren Reis <darren.r.reis@gmail.com>
2014-01-28 17:17:50 -0800

    bpsk and qpsk fixes

--- step1/SNR2EbN0.m
+++ step1/SNR2EbN0.m
@@ -1,4 +1,15 @@
 function [EbN0] = SNR2EbN0(SNR,symbolSize)
+% FUNCTION
+% this takes a given SNR ratio (in dB) and translates it into equivelant
+% Eb/No values
+
+% INPUTS
+% SNR - the db signal to noise ratio
+% symbolSize - the number of bits per symbol
+
+% OUTPUTS
+% EbN0 - the bit power over noise power
+
 
 EbN0 = SNR - 10*log10(symbolSize);
 
--- step1/step1_sim_qpsk.m
+++ step1/step1_sim_qpsk.m
@@ -6,20 +6,40 @@ Ts = 1;
 srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,400,Ts);
 SNR = [3 4 5 6 7 8 9 10 11 12 13 14 15 16];
 
+
+
 EbN0 = SNR2EbN0(SNR,2);
 
 %%QPSK simulation
 N= 1000;
+k = 2;  % bits per symbol
+
+% function checks
+modTester = '00011110';
+expectedI = [-1 -1 1 1];
+expectedQ = [-1 1 1 -1];
+[resultI,resultQ] = qpsk_mod(modTester,length(modTester)/k);
+failedI = sum(expectedI-resultI);
+failedQ = sum(expectedQ-resultQ);
+disp(['Number of failed I modulation symbols:' num2str(failedI)]);
+disp(['Number of failed Q modulation symbols:' num2str(failedQ)]);
+
+demodTesterI = expectedI;
+demodTesterQ = expectedQ;
+expectedBits = bin2dec(modTester);
+resultBits = qpsk_demod(demodTesterI,demodTesterQ);
+failedBits = sum(expectedBits - bin2dec(resultBits));
+disp(['Number of failed demodulated bits:' num2str(failedBits)]);
 
 %random bit generation
 bits = random_bit_generator(N);
 
 %mapping to symbols
-[quadrature, inphase] = qpsk_mod(bits,N/2);
+[quadrature, inphase] = qpsk_mod(bits,N/k);
 
 %mapping symbols to signals
-impulse_train_quad = impulse_train(overSampleSize,N/2,quadrature);
-impulse_train_inphase = impulse_train(overSampleSize,N/2,inphase);
+impulse_train_quad = impulse_train(overSampleSize,N/k,quadrature);
+impulse_train_inphase = impulse_train(overSampleSize,N/k,inphase);
 transmit_quad = conv(impulse_train_quad,srrc,'same');
 transmit_inphase = conv(impulse_train_inphase,srrc,'same');
 
@@ -46,7 +66,7 @@ for i=1:length(SNR)
 
     %BER/SER calculation
     ber(i) = BER(bits(3:N),output_bits(3:N));    
-    ser(i) = SER(bits,output_bits,2);
+    ser(i) = SER(bits,output_bits,k);
     %SER/BER theoretical calculation (BER=SER due to grey coding)
     a = 10^(EbN0(i)/10);
     ber_theo(i) = 2*qfunc(sqrt(2*a))-qfunc(sqrt(2*a))^2;
@@ -60,5 +80,8 @@ semilogy(SNR,ser,'r');
 hold on;
 %semilogy(SNR,ser, 'b');
 semilogy(SNR,ber_theo, 'g');
-ylabel('');
-xlabel('');
+title(['Comparison of Theoretical and Experimental',...
+    sprintf('\nQPSK Bit Error Rates')]);
+ylabel('Bit Error Rate (%)');
+xlabel('Signal To Noise (dB)');
+
