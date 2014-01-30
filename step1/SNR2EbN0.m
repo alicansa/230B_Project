530e23954454701ce76af43b09e3358ae9698817
alicansa <alicansa@gmail.com>
2014-01-29 21:19:21 -0800

    -

--- step1/SNR2EbN0.m
+++ step1/SNR2EbN0.m
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 function [EbN0] = SNR2EbN0(SNR,symbolSize,B)
-=======
-function [EbN0] = SNR2EbN0(SNR,symbolSize)
 % FUNCTION
 % this takes a given SNR ratio (in dB) and translates it into equivelant
 % Eb/No values
@@ -12,8 +10,6 @@ function [EbN0] = SNR2EbN0(SNR,symbolSize)
 
 % OUTPUTS
 % EbN0 - the bit power over noise power
-
->>>>>>> ced5907d9eaa7407cb1f501b7bc96fb0d4c31acf
 
 EbN0 = SNR - 10*log10(symbolSize/B);
 
--- step1/step1_sim_QAM16.m
+++ step1/step1_sim_QAM16.m
@@ -1,17 +1,17 @@
 close all;
 clear all;
 overSampleSize = 4;
-rollOffFactor = 1;
+rollOffFactor = 0;
 Ts = 1;
 S=10; %average signal power for 16-QAM
 B = rollOffFactor*(1/(2*Ts)) + 1/(2*Ts);
-srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,400,Ts);
+srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,4000,Ts);
 SNR = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20];
 
 EbN0 = SNR2EbN0(SNR,4,B);
 
-%%QPSK simulation
-N= 30000;
+%%QAM-16 simulation
+N= 10000;
 
 %random bit generation
 bits = random_bit_generator(N);
@@ -25,7 +25,6 @@ impulse_train_inphase = impulse_train(overSampleSize,N/4,inphase);
 transmit_quad = conv(impulse_train_quad,srrc,'same');
 transmit_inphase = conv(impulse_train_inphase,srrc,'same');
 
-
 %loop this section for BER vs SNR graphs
 
 ber = zeros(1,length(SNR));
@@ -35,10 +34,6 @@ for i=1:length(SNR)
     received_quad = awgn_channel(transmit_quad,SNR(i),S);
     received_inphase = awgn_channel(transmit_inphase,SNR(i),S);
     
-    
-%      received_quad = awgn(transmit_quad,SNR(i));
-%      received_inphase = awgn(transmit_inphase,SNR(i));
-%     
     %matched filter
     matched_output_quad = conv(received_quad,srrc,'same');
     matched_output_inphase = conv(received_inphase,srrc,'same');
@@ -46,26 +41,25 @@ for i=1:length(SNR)
     %sampler
     sampled_quad = sampler(matched_output_quad,overSampleSize,Ts);
     sampled_inphase = sampler(matched_output_inphase,overSampleSize,Ts);
-    scatterplot(sampled_inphase + j*sampled_quad);
+   % scatterplot(sampled_inphase + j*sampled_quad);
+   
     %decision
     output_bits = QAM_16_demod(sampled_inphase,sampled_quad);
 
     %BER/SER calculation
-    ber(i) = BER(bits(5:N),output_bits(5:N));    
-    ser(i) = SER(bits,output_bits,4);
+    ser(i) = SER(bits(5:N),output_bits(5:N),4);
     %SER/BER theoretical calculation (BER=SER due to grey coding)
     a = 10^(EbN0(i)/10);
-    %ber_theo(i) = 3*qfunc(sqrt(2*a))-(9/4)*qfunc(sqrt(2*a))^2;
-    ber_theo(i) = (1/4)*3/2*erfc(sqrt(4*0.1*a));
+    ser_theo(i) = 3*qfunc(sqrt((2)*a))-(9/4)*qfunc(sqrt((2)*a))^2;
 end
 
 
 %plot theoretical/simulation BER vs SNR graph
 
 h=figure;
-semilogy(SNR,ser,'r');
+semilogy(SNR,ser, 'ko');
 hold on;
-semilogy(SNR,ser, 'b');
-semilogy(EbN0,ber_theo, 'g');
-ylabel('');
-xlabel('');
+semilogy(SNR,ser_theo, 'r');
+ylabel('Probability of Symbol Error');
+xlabel('SNR(dB)');
+legend('Simulation','Theory');
