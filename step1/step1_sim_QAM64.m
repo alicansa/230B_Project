78b0b17d9d23282f22664ea829c4485a8c902610
Darren Reis <darren.r.reis@gmail.com>
2014-02-01 12:00:55 -0800

    blah

--- step1/step1_sim_QAM64.m
+++ step1/step1_sim_QAM64.m
@@ -8,6 +8,7 @@ B = rollOffFactor*(1/(2*Ts)) + 1/(2*Ts);
 srrc = sqrt_raised_cosine(overSampleSize,rollOffFactor,400,Ts);
 SNR = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20];
 
+
 EbN0 = SNR2EbN0(SNR,6,B);
 
 %%QPSK simulation
