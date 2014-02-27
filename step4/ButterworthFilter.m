647cc4d33b361ce827a2c1960ffb9ab23ee4c9d6
Darren Reis <darren.r.reis@gmail.com>
2014-02-27 11:14:12 -0800

    more latex

--- step4/ButterworthFilter.m
+++ step4/ButterworthFilter.m
@@ -1,5 +1,16 @@
 function [output] = ButterworthFilter(order,wc,data)
-[b,a] = butter(order,wc);
+% FUNCTION - create and execute passing a signal through a butterworth
+%   filter
+
+% INPUTS
+% order - the order is the number of poles in the transfer function
+% wc - the normalized cuttoff freq
+% data - the filter input waveform
+
+% OUTPUTS
+% output - the output from running the data through the BF
+
+[b,a] = butter(order,wc);  % returns as [num, denom] of T.F
 output = filter(b,a,data);
 end
 
